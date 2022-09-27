c69d4780e014a69b64682ec0dbfe497e872628df
Rene90 <rob_rene199027@hotmail.com>
2022-09-26 19:23:02 -0600

    fetch a fotos

--- 04APINASA/index.js
+++ 04APINASA/index.js
@@ -30,20 +30,57 @@ async function apinasa(url){
 
 
 //apinasa(apiNasa)
-let im
-let idim 
-async function fotosMarte(key,robot,camara,dia){
 
+async function fotosMarte(key,robot,camara,dia){
+    var contenedor =document.getElementById("contenedorFoto")
+    var fotodia = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+    var fotodiajson = await fotodia.json()
     var urlmarte = `https://api.nasa.gov/mars-photos/api/v1/rovers/${robot}/photos?sol=${dia}&camera=${camara}&api_key=${key}`
     const respuestaurl = await fetch(urlmarte)
     var respjson = await respuestaurl.json()
     console.log(respjson)
     var listafotos =respjson.photos
     console.log(listafotos)
-    listafotos.forEach((elemento,indice,arreglo)=>{
+    contenedor.innerHTML = ""
+    if (listafotos.length==0){
+        console.log(fotodiajson)
+        //contenedor.classList.remove("mt-4")
+        //contenedor.classList.add("mt-12")
+        contenedor.innerHTML = `
+        <div class="card" style="width: 100%;">
+        <img class="card-img-top" src=${fotodiajson.url} alt=${fotodiajson.media_type}>
+        <div class="card-body">
+          <h5 class="card-title">${fotodiajson.title}</h5>
+          <p class="card-text">${fotodiajson.explanation}</p>
+          
+        </div>
+      </div>
         
-        im = elemento.img_src
-        idim = elemento.id
+        `
+
+    }
+    
+
+    listafotos.forEach((elemento,indice,arreglo)=>{
+        contenedor.innerHTML += `
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src=${elemento.img_src} alt=${elemento.id}>
+        <div class="card-body">
+          <h5 class="card-title">${elemento.rover.name}</h5>
+          <p class="card-text">${elemento.camera.full_name}</p>
+          
+        </div>
+      </div>
+        
+        
+        
+        `
+
+
+
+
+
+    
 
     })
 }
@@ -55,6 +92,7 @@ async function traerfoto(){
     let sensorv = sensor.value
     let diasolarv = diasolar.value
     console.log(roverv, sensorv, diasolarv)
+    await fotosMarte(llave,roverv,sensorv,String(diasolarv))
     //var contenedor =document.getElementById("contenedorFoto")
     //await fotosMarte(llave,"curiosity","FHAZ","1000")
     
