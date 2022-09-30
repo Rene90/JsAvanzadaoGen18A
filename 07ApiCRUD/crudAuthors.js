16bbcb2e3da850415e67bfc6b33967eb5700e491
Jesua Luján <code.masterqro@gmail.com>
2022-09-29 19:37:37 -0500

    request type get

--- 07ApiCRUD/crudAuthors.js
+++ 07ApiCRUD/crudAuthors.js
@@ -1,1 +1,27 @@
 //* VAMOS A TENER NUESTRA LÓGICA 
+//* DOCUMENTACIÓN: https://goodreads-devf-aaron.herokuapp.com/docs/
+//* URI DE LA API: https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/
+
+//? 1) TRAER A REQUEST QUE ACABAMOS DE INSTALAR 
+const request = require('request')
+
+//? 2) TENER NUESTRO ENDPOINT 
+const URI = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/'
+
+//? LISTAR A TODOS LOS AUTORES (authors)
+const listAuthors = ( ) => {
+    request.get(URI,(error,response,body)=>{
+        //validamos nuestra petición
+        if(response.statusCode === 200){
+            const authors = JSON.parse(body)
+            console.log(authors)
+        }else{
+            console.log(response.statusCode , response.statusMessage)
+        }                 //! 404                  Not Found
+    })
+}
+
+
+  module.exports = {
+    listAuthors,
+  } 
--- 07ApiCRUD/main.js
+++ 07ApiCRUD/main.js
@@ -1,1 +1,7 @@
 //* VAMOS A EJECUTAR LA LÓGICA 
+//* MAIN ES NUESTRO JS PRINCIPAL
+
+const goodReadsCrud = require ('./crudAuthors')
+
+//* LISTAR A TODOS LOS AUTORES 
+goodReadsCrud.listAuthors()
